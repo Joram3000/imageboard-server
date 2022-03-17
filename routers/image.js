59c567bf0841049f11320518b4016b5542f6852d
Joram3000 <98752947+Joram3000@users.noreply.github.com>
2022-03-17 14:36:58 +0100

    ZIJN NU ALLE CHANGES MEE

---
+++ routers/image.js
@@ -0,0 +1,35 @@
+const Image = require("../models").image;
+
+const { Router } = require("express");
+const router = new Router();
+
+//POST EEN PLAATJE TITLE EN URL
+router.post("/images", async (req, res, next) => {
+  try {
+    const url = req.body.url;
+    if (!url || url === " ") {
+      res.status(400).send("Je moet wel een goede url opgeven hondezak");
+    } else {
+      const imagePost = await Image.create(req.body);
+      res.json(imagePost);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+//SHOW ALLE IMAGES
+router.get("/images", async (req, res, next) => {
+  const plaatjes = await Image.findAll();
+  res.json(plaatjes);
+});
+
+//SHOW 1 IMAGE
+router.get("/images/:id", async (req, res, next) => {
+  const plaatjeId = parseInt(req.params.id);
+  const plaatje = await Image.findByPk(plaatjeId);
+
+  res.json(plaatje.url); //showt alleen maar de URL
+});
+
+module.exports = router;
