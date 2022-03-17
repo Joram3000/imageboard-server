7eabf76db1b205f719fc1230549ad89344f1c79b
Joram3000 <98752947+Joram3000@users.noreply.github.com>
2022-03-17 12:29:19 +0100

    letsgo

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
